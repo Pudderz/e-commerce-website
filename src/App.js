24770c094636ad75561a8431ac7d866c3d767111
Pudderz <mpudney6@gmail.com>
2021-01-27 14:52:40 +0000

    Added multiple pages (react-router))

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 
 import './App.css';
+import { StorePage } from './pages/StorePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { ProductPage } from './Template/ProductPage';
+import { FindAStore } from './pages/FindAStore';
+import { FrontPage } from './pages/FrontPage';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Basket } from './pages/Basket';
+import { LoginPage } from './pages/LoginPage';
+import { Profile } from './pages/Profile';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+
+          <div>
+        <Header/>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/store">
+            <StorePage/>
+          </Route>
+          <Route path="/product">
+            <ProductPage/>
+          </Route>
+          <Route path="/findastore">
+            <FindAStore/>
+          </Route>
+          <Route path="/basket">
+            <Basket/>
+          </Route>
+          <Route path="/login">
+            <LoginPage/>
+          </Route>
+          <Route path="/profile">
+            <Profile/>
+          </Route>
+          <Route path="/">
+            <FrontPage/>
+          </Route>
+        </Switch>
+      </div>
+      
+      <Footer/>
+    </Router>
+      
+      </div>
   );
 }
 
