d8102d0ca0506e2dd2c59525bbc9c2dba78b2b26
Pudderz <mpudney6@gmail.com>
2021-01-27 15:04:33 +0000

    Created basic layour for front page

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,30 +1,47 @@
-import React from 'react'
-
-
+import React from "react";
+import { RecentlyViewed } from "../components/RecentlyViewed";
 
 //Home Page
 
-
-// Header
-
-//Featured Section
+//Featured Section/Banner section
 
 // Top Products
 
 // Instagram Section
 
-//
-
-// Subscribe to newsletter
-
-// footer
+// categories section
 
 export const FrontPage = () => {
-    return (
-
-
-        <div>
-            
+  return (
+    <div>
+      <div style={{ backgroundColor: "grey", height: "600px", width: "100%" }}>
+        Banner
+      </div>
+      <div style={{ backgroundColor: "#eee", height: "500px", width: "100%" }}>
+        Featured products
+      </div>
+      <div style={{ backgroundColor: "#fff", height: "500px", width: "100%" }}>
+        Categories Section
+        <div style={{ display: "flex", justifyContent: "space-around", margin:'20px 0' }}>
+          <div
+            style={{ backgroundColor: "#ddd", height: "400px", width: "250px" }}
+          >
+            Running
+          </div>
+          <div
+            style={{ backgroundColor: "#ddd", height: "400px", width: "250px", marginTop:'20px' }}
+          >
+            Casual
+          </div>
+          <div
+            style={{ backgroundColor: "#ddd", height: "400px", width: "250px" }}
+          >
+            Hiking
+          </div>
         </div>
-    )
-}
+      </div>
+          <RecentlyViewed/>
+
+    </div>
+  );
+};
