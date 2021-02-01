5c1d6f8149d5812947b504be38c9c4d0b99e101d
Pudderz <mpudney6@gmail.com>
2021-02-01 20:35:10 +0000

    Updated Product page with new components and changed styles

--- src/Template/ProductPage.js
+++ src/Template/ProductPage.js
@@ -1,33 +1,47 @@
-import React, {useEffect, useState} from "react";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
+import {
+  Breadcrumbs,
+  Button,
+  Paper,
+  Tabs,
+  Typography,
+} from "@material-ui/core";
+import React, { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
 import { ProductImages } from "../components/ProductImages";
+import { ProductTabs } from "../components/ProductTabs";
 import { RecentlyViewed } from "../components/RecentlyViewed";
 import { commerce } from "../lib/commerce";
 import { addToHistory } from "../lib/localStorage";
-export const ProductPage = () => {
-  const [product, setProduct]= useState({})
-  const [cart, setCart] = useState({})  
+import { SnackbarProvider, useSnackbar } from "notistack";
+import { RecentProducts } from "../components/RecentProducts";
+import { ReviewProduct } from "../components/ReviewProduct";
+import { CartContext } from "../context/CartContext";
+import { SelectSize } from "../components/SelectSize";
 
-  
-  const fetchItem =(id)=>{
-      commerce.products
-        .retrieve(id)
-        .then((cart) => {
-          setProduct(cart);
-          
-        })
-        .catch((error) => {
-          console.error("There was an error fetching the cart", error);
-        });
-  }
+export const ProductPage = () => {
+  const [product, setProduct] = useState({});
+  // const [cart, setCart] = useState({});
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const { cart, changeCart } = useContext(CartContext);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const fetchItem = (id) => {
+    commerce.products
+      .retrieve(id)
+      .then((cart) => {
+        setProduct(cart);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the cart", error);
+      });
+  };
 
   const fetchCart = () => {
     commerce.cart
       .retrieve()
       .then((cart) => {
-        setCart(cart );
-        
+        changeCart(cart);
       })
       .catch((error) => {
         console.error("There was an error fetching the cart", error);
@@ -37,80 +51,158 @@ export const ProductPage = () => {
   useEffect(() => {
     let search = window.location.search;
     let params = new URLSearchParams(search);
-    let id = params.get('id');
+    let id = params.get("id");
     fetchItem(id);
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
   useEffect(() => {
     console.log(product);
-  }, [product])
-
+  }, [product]);
 
   useEffect(() => {
     fetchCart();
 
-    return ()=>{
+    return () => {
       addToHistory(product);
-    }
+    };
   }, [product]);
 
-  const handleAddToCart= (item, quantity=1)=> {
-    commerce.cart.add(item.id, quantity).then((item) => {
-      setCart(item.cart)
-    }).catch((error) => {
-      console.error('There was an error adding the item to the cart', error);
-    });
+  const handleAddToCart = (item, quantity = 1) => {
+    commerce.cart
+      .add(item.id, quantity)
+      .then((item) => {
+        changeCart(item.cart);
+        enqueueSnackbar("Item added to the basket", { variant: "success" });
+      })
+      .catch((error) => {
+        console.error("There was an error adding the item to the cart", error);
+        enqueueSnackbar("Item could not be added to the basket", {
+          variant: "error",
+        });
+      });
   };
 
-
+  const handleTab = (tabNumber) => {
+    setCurrentTab(tabNumber);
+  };
   return (
-    <>
-      <p>BreadCrumb</p>
-<button>Back</button>
-      <div style={{display:'flex', margin:'auto', width:'fit-content'}}>
+    <div style={{ padding: "0 20px" }}>
+
+      <div
+        style={{
+          display: "flex",
+          margin: "auto",
+          width: "fit-content",
+          textAlign: "start",
+          marginTop: "50px",
+        }}
+      >
         <div>
           {/* Product Images */}
-          <ProductImages
-          images = {product.assets}
-          />
+          <ProductImages images={product.assets} />
         </div>
         <div>
-        <div>
-          <h1>{product.name}</h1>
-          {/* +Review bar */}
-          <h2>{product?.price?.formatted_with_symbol}</h2>
-          <p>Number of Stock left - {product.quantity}</p>
-          <p>Description - With read more button</p>
-        <p>Available sizes:</p>
-        <p>Colours Available:</p>
-          {/* Available sizes section */}
-          <input type="text" />
-          <button onClick={()=>handleAddToCart(product, 1)}>ADD TO BASKET</button>
-          <button>BUY NOW</button>
-        </div>
-      
+          <div>
+            <h1>{product.name}</h1>
+            {/* +Review bar */}
 
-      {/* tabbed sections with information  */}
-      
+            {/*     font-size: 16px;
+    margin: 0; */}
+            <h2>{product?.price?.formatted_with_symbol}</h2>
+            <p style={{color:'green'}}>In Stock ({product.quantity} left)</p>
+            
+            <label htmlFor="sizes" style={{fontWeight:'bold'}}>Select size</label>
+            <SelectSize availableSizes={[4,6, 8 ,9]}/>
+            {/* <select
+              name="sizes"
+              id="sizes"
+              style={{ width: "100%", height: "30px" }}
+            >
+              <option disabled>Select a size</option>
+              <option value="5">UK 5</option>
+              <option value="6">UK 6</option>
+              <option value="7">UK 7</option>
+              <option value="8">UK 8</option>
+            </select> */}
+            <a href="#!">Size Guide</a>
+            {/* <p>Colours Available:</p> */}
+            {/* Available sizes section */}
+            {/* <input type="text" />
+            <button>{"<"}</button>
+            <button>{">"}</button> */}
+<hr/>
+            <Button
+              onClick={() => handleAddToCart(product, 1)}
+              variant="contained"
+            >
+              ADD TO BASKET
+            </Button>
+            <Button variant="contained">BUY NOW</Button>
+          </div>
+
+          {/* tabbed sections with information  */}
         </div>
-    
       </div>
-<div style={{margin:'auto', width:'fit-content'}}>
-        <div>{/* Buttons for tab at top */}</div>
-        <div style={{display:'flex'}}>
-          <button>descriptions</button>
-          <button>Reviews</button>
-          <button>Size Guide</button>
-          <button>Delivery & Returns</button>
+
+      {/* sticky header for when top section is not in view (intersection observer)
+       */}
+
+      <div
+        style={{
+          top: "0px",
+          position: "sticky",
+          height: "fit-content",
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "white",
+          zIndex: "2",
+          margin: "0 -20px",
+          padding: " 0",
+          width: "81%",
+        }}
+      >
+        <Breadcrumbs aria-label="breadcrumb" style={{ margin: "1em" }}>
+          <Link to="/">Home</Link>
+          <Link to="/">Store</Link>
+          <Typography>{product?.name}</Typography>
+        </Breadcrumbs>
+
+        <div style={{ display: "flex" }}>
+          <img src={product?.assets?.[0]?.url} height="60px" alt="" />
+          <h4 style={{ margin: "1em" }}>{product.name}</h4>
+          <p>{product?.price?.formatted_with_symbol}</p>
         </div>
-        <div>
-          <div>descriptions</div>
-          <div>Reviews</div>
-          <div>Size Guide</div>
-          <div>Delivery & Returns</div>
+
+        <div
+          style={{
+            display: "flex",
+            minWidth: "30%",
+            width: "fit-content",
+            justifyContent: "space-between",
+          }}
+        >
+          <select
+            name="sizes"
+            id="sizes"
+            style={{ margin: "1em", height: "30px" }}
+          >
+            <option disabled>Select a size</option>
+            <option value="5">UK 5</option>
+            <option value="6">UK 6</option>
+            <option value="7">UK 7</option>
+            <option value="8">UK 8</option>
+          </select>
+
+          <Button>Add to Basket</Button>
+          <Button>Save</Button>
         </div>
       </div>
-      <RecentlyViewed/>
-    </>
+      <hr />
+      <ProductTabs />
+      <hr />
+      <ReviewProduct />
+      <RecentlyViewed />
+    </div>
   );
 };
