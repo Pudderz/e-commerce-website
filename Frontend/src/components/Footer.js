de483f6760e75f3fc0fbfcdb74751bd3c8df9e60
Pudderz <mpudney6@gmail.com>
2021-02-12 00:09:29 +0000

    removed unnecessary code from footer component

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -1,43 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
-    return (
-        <div>
-            <div>Subscribe to our newsletter
-            <input type="text" />    
-            </div>
-            
-        <footer className="grid">
-          <hr
-            style={{ width: "100%", maxWidth: "1300px", margin: "24px auto 0" }}
-          />
-          <div
-            style={{ margin: "auto", padding: "20px" }}
-            className="selection"
-          >
-            {/* <CategorySelection changeCategory={this.changeCategory}/> */}
-          </div>
-          <hr style={{ width: "100%", maxWidth: "1300px", margin: "auto" }} />
-          <div
-            className="flexFooter"
-            style={{
-              display:'flex',
-              justifyContent:'space-between',
-              width: "100%",
-              maxWidth: "1300px",
-              margin: "auto",
-              padding: "10px",
-              flexWrap: 'wrap',
-            }}
-          >
-            <Link to="/">Home</Link>
-            <Link to="/store">About </Link>
-            <Link to="/findastore">Find a Store</Link>
-            <Link to="/store">Products</Link>
-          </div>
-        </footer>
+  return (
+    <div style={{ maxWidth: "100%", position: "relative" }}>
+ 
+      <footer className="grid">
+        <hr
+          style={{ maxWidth: "100%", maxWidth: "1300px", margin: "24px auto 0" }}
+        />
 
+        <div
+          className="flexFooter"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+            maxWidth: "1300px",
+            margin: "auto",
+            padding: "10px",
+            flexWrap: "wrap",
+            boxSizing:'border-box'
+          }}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/store">About </Link>
+          <Link to="/findastore">Find a Store</Link>
+          <Link to="/store">Products</Link>
         </div>
-    )
-}
+      </footer>
+    </div>
+  );
+};
