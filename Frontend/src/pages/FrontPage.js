1c179d526f6a6c9acc68828763bb59be6a183cde
Pudderz <mpudney6@gmail.com>
2021-02-12 00:05:02 +0000

    updated front-page to be more responsive

--- Frontend/src/pages/FrontPage.js
+++ Frontend/src/pages/FrontPage.js
@@ -9,6 +9,9 @@ import { MostPopular } from "../components/MostPopular";
 import { RecentProducts } from "../components/RecentProducts";
 import { Link } from "react-router-dom";
 import { FollowInstagram } from "../components/FollowInstagram";
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import PaymentIcon from '@material-ui/icons/Payment';
+import { GetReviews } from "../components/GraphQl/GetReviews";
 //Home Page
 
 //Featured Section/Banner section
@@ -53,14 +56,19 @@ export const FrontPage = () => {
         Banner
       </div>
       <MostPopular />
-      
+
+
+{/* Category Selection */}
       <div style={{ backgroundColor: "#fff", height: "800px", width: "100%" , paddingTop:'100px'}}>
         <h2>Categories</h2>
         <div
           style={{
             display: "flex",
             justifyContent: "space-around",
+            gap:'30px',
             margin: "20px 0",
+            padding:'40px',
+            boxSizing:'border-box'
           }}
         >
           <div
@@ -125,17 +133,33 @@ export const FrontPage = () => {
       </div>
 <div
         style={{
-          backgroundColor: "#ddd",
+          
           height: "300px",
-          width: "100%",
-          marginTop: "20px",
+          width: "90%",
+          margin: "20px auto 0",
           display: "flex",
           justifyContent: "space-around",
         }}
       >
-        <div>Next day delivery in the uk</div>
-        <div>30 day return refund</div>
-        <div>customer support</div>
+        <div style={{display:'grid', height:'fit-content', maxWidth:'300px'}}>
+          <div><LocalShippingIcon style={{ fontSize: 70 }}/></div>
+          <h3>Next day delivery in the uk</h3>
+          <p>If you order before 3pm</p>
+          
+          </div>
+        <div style={{display:'grid', height:'fit-content', maxWidth:'300px'}}>
+          <div><PaymentIcon  style={{ fontSize: 70 }}/></div>
+          <h3>30 day return refund</h3>
+          <p>Did't not get the right size or colour. 30 days refund no questions asked.</p>
+
+          </div>
+        <div style={{display:'grid', height:'fit-content', maxWidth:'300px'}}>
+          <div><PaymentIcon style={{ fontSize: 70 }} /></div>
+          <h3>24/7 Customer Support</h3>
+          <p></p>
+
+          </div>
+        
       </div>
         
 
