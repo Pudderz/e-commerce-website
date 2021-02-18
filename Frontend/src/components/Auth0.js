926a39d63853e48017593c6783e46461ec384b3c
Pudderz <mpudney6@gmail.com>
2021-02-18 12:50:17 +0000

    Added validation to backend
    Sends Authentication JWT  to back-end on each query request. That gets validated using jwt.verify()

--- Frontend/src/components/Auth0.js
+++ Frontend/src/components/Auth0.js
@@ -2,6 +2,13 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+
+const requestedScopes = [
+  'write:review',
+  'read:review',
+];
+
+
 export const Auth0 = (props) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -12,6 +19,7 @@ export const Auth0 = (props) => {
     history.push(appState?.returnTo || window.location.pathname);
   };
 
+
   return (
     <div>
       <Auth0Provider
@@ -19,6 +27,9 @@ export const Auth0 = (props) => {
         clientId={clientId}
         redirectUri={window.location.origin}
         onRedirectCallback={onRedirectCallback}
+        scope={requestedScopes.join(' ')}
+        audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+        responseType={'token id_token'}
       >
         {props.children}
       </Auth0Provider>
--- Frontend/src/context/CartContext.js
+++ Frontend/src/context/CartContext.js
@@ -1,39 +1,55 @@
-import React, {useEffect, useState} from 'react' 
-import { clearCart, fetchCart, handleAddToCart, removeItemFromCart, updateCartQty } from '../lib/commerce';
+import React, { useEffect, useState } from "react";
+import {
+  clearCart,
+  fetchCart,
+  handleAddToCart,
+  removeItemFromCart,
+  updateCartQty,
+} from "../lib/commerce";
 
+import { useAuth0 } from "@auth0/auth0-react";
 export const CartContext = React.createContext();
 
 export const CartContextProvider = (props) => {
-    const [cart, setCart] = useState({})
-    useEffect(() => {
-        fetchCart(setCart);
-    }, [])
+  const { getAccessTokenSilently } = useAuth0();
 
-    const addToCart = (item, quantity)=>{
-        handleAddToCart(item, quantity, setCart)
+  const [cart, setCart] = useState({});
+  useEffect(async () => {
+    fetchCart(setCart);
+    try {
+      const token = await getAccessTokenSilently();
+      console.log(token, getAccessTokenSilently());
+      if (token) {
+        localStorage.setItem("token", token);
+      }
+    } catch (err) {
+      console.log(err);
     }
+  }, []);
 
-    const removeItem = itemId =>{
-        removeItemFromCart(itemId,setCart);
-    }
+  const addToCart = (item, quantity) => {
+    handleAddToCart(item, quantity, setCart);
+  };
 
-    const handleNewItemQuantity = (id, newQty)=>{
-        updateCartQty(id, newQty, setCart);
-    }
-    return (
-            <CartContext.Provider value={{
-                changeCart:(value)=>setCart(value),
-                cart,
-                addToCart: (item, quantity)=>addToCart(item, quantity),
-                removeFromCart: (itemId)=> removeItem(itemId),
-                clearCart: ()=> clearCart(setCart),
-                updateQty: (itemId, newQty)=> handleNewItemQuantity(itemId, newQty)
-            }
+  const removeItem = (itemId) => {
+    removeItemFromCart(itemId, setCart);
+  };
 
-            }>
-                {props.children}
-            </CartContext.Provider>
-    )
-}
-
-
+  const handleNewItemQuantity = (id, newQty) => {
+    updateCartQty(id, newQty, setCart);
+  };
+  return (
+    <CartContext.Provider
+      value={{
+        changeCart: (value) => setCart(value),
+        cart,
+        addToCart: (item, quantity) => addToCart(item, quantity),
+        removeFromCart: (itemId) => removeItem(itemId),
+        clearCart: () => clearCart(setCart),
+        updateQty: (itemId, newQty) => handleNewItemQuantity(itemId, newQty),
+      }}
+    >
+      {props.children}
+    </CartContext.Provider>
+  );
+};
