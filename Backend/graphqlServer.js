d2f701ee9e2524cb491c9f73f4f0cbdfdd11caec
Pudderz <mpudney6@gmail.com>
2021-02-18 12:42:26 +0000

    Added mongoose and mongodb
    added mongodb database to backend server

--- Backend/graphqlServer.js
+++ Backend/graphqlServer.js
@@ -4,17 +4,51 @@ const PORT = 6969;
 const cors = require('cors');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./Schemas');
+const mongoose = require('mongoose');
+const isTokenValid = require('./Authenication/validate');
+let database = null;
+
+require("dotenv").config();
+const uri = `mongodb+srv://dbAdmin:${process.env.MONGODB_PASSWORD}@cluster0.s5qsx.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`;
+    console.log(uri)
+    
+const startDatabase =()=>{
+    
+         mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('db connection is okay');
+
+            if (!database) {
+            console.log('no database set')
+            }
+        }
+    }) 
+    
+}
 
 
+const context = async (req) => {
+    const db = await startDatabase();
+    const token = req.headers.authorization;
+    
+    return { db, token };
+  };
+
+
+
+ 
+  
 
 
 app.use(cors());
 app.use(express.json());
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP(async req=>({
     schema,
-    // rootValue: resolvers,
     graphiql: true,
-}))
+    context: () => context(req)
+})))
 
 
 app.listen(PORT, () => {
