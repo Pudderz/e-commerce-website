b3da5ea96b28ff4daea1a403c1a1e605badf9007
Pudderz <mpudney6@gmail.com>
2021-02-18 12:46:11 +0000

    querys now change mongodb database instead of JSON data

--- Backend/Schemas/TypeDefs/UserType.js
+++ Backend/Schemas/TypeDefs/UserType.js
@@ -1,4 +1,5 @@
 const graphql = require("graphql");
+const Review = require("../../models/review");
 const {
   GraphQLObjectType,
   GraphQLInt,
@@ -11,6 +12,7 @@ const ProductReviews = new GraphQLObjectType({
   name: "Review",
   fields:()=>({
     id: { type: GraphQLInt },
+    productId:{ type: GraphQLString },
     name: { type: GraphQLString },
     profileImage: { type: GraphQLString},
     rating: { type: GraphQLString },
@@ -25,16 +27,16 @@ const ProductType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLInt },
     productId: { type: GraphQLString },
+    productName: { type: GraphQLString },
     numOfReviews: { type: GraphQLInt },
     averageRating: { type: GraphQLString },
     allProductReviews: { 
       type: new GraphQLList(ProductReviews),
-      // args: { id: { type: GraphQLInt } },
-      // resolve(parent, args) {
-      //   return userData;
-      // }, 
+      resolve(parent, args){
+        return Review.find({productId: parent.productId})
+      }
     },
   }),
 });
 
-module.exports = ProductType;
+module.exports = {ProductType, ProductReviews};
--- Backend/Schemas/index.js
+++ Backend/Schemas/index.js
@@ -1,15 +1,18 @@
 const graphql = require("graphql");
+const mongoose = require("mongoose")
 const {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLInt,
   GraphQLString,
   GraphQLList,
+  GraphQLID,
+  GraphQLNonNull,
 } = graphql;
-const ProductType =require('./TypeDefs/UserType')
-const reviewData = require('../reviewDataExample.json');
-
-
+const { ProductType, ProductReviews } = require("./TypeDefs/UserType");
+const reviewData = require("../reviewDataExample.json");
+const Review = require("../models/review");
+const Product = require("../models/products");
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -18,9 +21,34 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(ProductType),
       args: { id: { type: GraphQLInt } },
       resolve(parent, args) {
-        return reviewData;
+        return Product.find({});
       },
     },
+    getAllReviews: {
+      type: new GraphQLList(ProductReviews),
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Review.find({});
+      },
+    },
+    getUserReviews: {
+      type: new GraphQLList(ProductReviews),
+      args: { id: { type: GraphQLID }, name: { type: GraphQLString } },
+      resolve(parent, args) {
+        return Review.find({ name: args.name });
+      },
+    },
+    getProduct:{
+      type: new GraphQLList(ProductType),
+      args: {
+        productId: { type: GraphQLString },
+        productName: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        console.log('finding');
+        return Product.find({ productName: args.productName });
+      },
+    }
   },
 });
 
@@ -30,31 +58,101 @@ const Mutation = new GraphQLObjectType({
     createProduct: {
       type: ProductType,
       args: {
-        productId: { type: GraphQLString },
-        lastName: { type: GraphQLString },
+        productId: { type: new GraphQLNonNull(GraphQLString) },
+        productName: { type: new GraphQLNonNull(GraphQLString) },
       },
-      resolve(parent, args) {
-        reviewData.push({
-          id: reviewData.length + 1,
-          firstName: args.firstName,
-          lastName: args.lastName,
+      resolve: async (parent, args, context) => {
+        // reviewData.push({
+        //   id: reviewData.length + 1,
+        //   firstName: args.firstName,
+        //   lastName: args.lastName,
+        // });
+        let product = new Product({
+          productId: args.productId,
+          productName: args.productName,
+          numOfReviews: 0,
+          averageRating: 0,
         });
-        return args;
+
+        return product.save();
       },
     },
-    createReview:{
+    createReview: {
       type: ProductType,
-      args:{
-        productId:{ type: GraphQLString },
-        name:{ type: GraphQLString },
-        profileImage:{ type: GraphQLString },
-        rating:{ type: GraphQLString },
-        descriptionTitle:{ type: GraphQLString },
-        description:{ type: GraphQLString },
+      args: {
+        productId: { type: GraphQLString },
+        productName: { type: GraphQLString },
+        name: { type: GraphQLString },
+        profileImage: { type: GraphQLString },
+        rating: { type: GraphQLString },
+        descriptionTitle: { type: GraphQLString },
+        description: { type: GraphQLString },
       },
-      resolve(parent,args){
-        let i = 0;
-        let match = false;
+      resolve: async (parent, args, context) => {
+
+        let review = new Review({
+          productId: args.productId,
+          productName: args.productName,
+          name: args.name,
+          profileImage: args.profileImage,
+          rating: args.rating,
+          descriptionTitle: args.descriptionTitle,
+          description: args.description,
+        });
+        
+
+        let product;
+        await Product.findOne({ productId: args.productId }, function (err, obj) {
+          
+          product = obj;
+          
+        });
+        
+
+        console.log(product);
+        console.log(!!product);
+         console.log(mongoose.connection.readyState=== 1);
+         console.log(typeof product === 'undefined');
+         console.log(product === product);
+
+        if((typeof product === 'undefined' || !product ) && mongoose.connection.readyState=== 1){
+          console.log(`${ args.productId} not found`)
+
+          product = new Product({
+            productId:args.productId,
+            productName:args.productName,
+            numOfReviews: 1,
+            averageRating: args.rating,
+
+          })
+
+          product.save();
+
+        }else{
+          
+          console.log(!(typeof product === 'undefined'))
+          if(!(typeof product === 'undefined')){
+          console.log(`${ args.productId} found`)  
+const rating = (1 * product.averageRating * product.numOfReviews +
+            1 * args.rating) /
+          (product.numOfReviews + 1);
+          console.log(rating);
+          console.log(product.numOfReviews + 1);
+          const numOfReviews = product.numOfReviews + 1;
+          await Product.updateOne({productId:args.productId},
+            {"$set":
+            {
+            numOfReviews:  numOfReviews,
+            averageRating: `${rating}`
+            },
+
+          }
+
+          )
+          }
+          
+        }
+
         // for(product in reviewData){
         //   if(product.productId === args.productId){
         //     product.allProductReviews.push({
@@ -64,33 +162,41 @@ const Mutation = new GraphQLObjectType({
         //       descriptionTitle:args.description,
         //       description: args.descriptionTitle,
         //     })
-            
-            
+
         //     reviewData[i].averageRating = (reviewData[i].averageRating*reviewData[i].numOfReviews+args.rating/(reviewData[i].numOfReviews+1))
         //     reviewData[i].numOfReviews ++;
         //     match = true;
         //     break;
-        //   } 
+        //   }
         //   i++;
         // }
         // if(!match){
-          reviewData[0].allProductReviews.push({
-            id:reviewData[0].allProductReviews.length+1,
-            name:args.name,
-            rating: args.rating,
-            descriptionTitle:args.description,
-            description: args.descriptionTitle,
-          })
+
+        //   console.log(`is Authenicated - ${!error}`)
+        //   console.log(error)
+        // if(!error){
+
+        //   reviewData[0].allProductReviews.push({
+        //   id: reviewData[0].allProductReviews.length + 1,
+        //   name: args.name,
+        //   rating: args.rating,
+        //   descriptionTitle: args.description,
+        //   description: args.descriptionTitle,
+        //   profileImage: args.profileImage,
+        // });
+        // // }
+
+        // reviewData[0].averageRating =
+        //   (1 * reviewData[0].averageRating * reviewData[0].numOfReviews +
+        //     1 * args.rating) /
+        //   (reviewData[0].numOfReviews + 1);
+        // reviewData[0].numOfReviews = reviewData[0].allProductReviews.length;
+        // console.log("added review");
         // }
-        console.log(reviewData[0].averageRating ,reviewData[0].numOfReviews )
-        console.log((1*reviewData[0].averageRating*reviewData[0].numOfReviews)+1*args.rating)
-        console.log(((1*reviewData[0].averageRating*reviewData[0].numOfReviews)+1*args.rating)/(1*reviewData[0].numOfReviews+1))
-        reviewData[0].averageRating = ((1*reviewData[0].averageRating*reviewData[0].numOfReviews)+1*args.rating)/(reviewData[0].numOfReviews+1)
-            reviewData[0].numOfReviews= reviewData[0].allProductReviews.length;
-            console.log('added review')
-        return args;
-      }
-    }
+
+        // return (!error)? args: null;
+      },
+    },
   },
 });
 
