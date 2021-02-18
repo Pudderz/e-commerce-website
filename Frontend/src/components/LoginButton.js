bdd0625bf93191bb5a8e3ace08a571256b3f9440
Pudderz <mpudney6@gmail.com>
2021-02-18 13:05:39 +0000

    Updated login and logout button with material ui

--- Frontend/src/components/LoginButton.js
+++ Frontend/src/components/LoginButton.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {useAuth0} from '@auth0/auth0-react'
+import { Button } from '@material-ui/core';
 export const LoginButton = () => {
 
     const {loginWithRedirect} = useAuth0();
     return (
         <div>
-           <button
+            
+           <Button
            onClick={()=>loginWithRedirect({screen_hint: 'signup'})}
-           >Log in</button> 
+           >Log in</Button> 
         </div>
     )
 }
--- Frontend/src/components/LogoutButton.js
+++ Frontend/src/components/LogoutButton.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {useAuth0} from '@auth0/auth0-react'
+import { Button } from '@material-ui/core';
 export const LogoutButton = () => {
 
     const {logout} = useAuth0();
     return (
         <div>
-           <button
+           <Button
            onClick={()=>logout({returnTo:window.location.origin})}
-           >Log out</button> 
+           >Log out</Button> 
         </div>
     )
 }
