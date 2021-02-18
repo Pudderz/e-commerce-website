4437e9099b8986a3f73da61b7a048c19f6b5b2c5
Pudderz <mpudney6@gmail.com>
2021-02-18 13:07:21 +0000

    Updated style of header component

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
@@ -6,11 +6,16 @@ import { Avatar, Badge, Button, IconButton, Tooltip } from "@material-ui/core";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "./LoginButton";
-import { Switch } from '@material-ui/core';
-import Popover from '@material-ui/core/Popover';
+import { Switch } from "@material-ui/core";
+import Popover from "@material-ui/core/Popover";
 import { LogoutButton } from "./LogoutButton";
-import { commerce, fetchCart, handleAddToCart, updateCartQty } from "../lib/commerce";
-import DeleteIcon from '@material-ui/icons/Delete';
+import {
+  commerce,
+  fetchCart,
+  handleAddToCart,
+  updateCartQty,
+} from "../lib/commerce";
+import DeleteIcon from "@material-ui/icons/Delete";
 /* 
 HEADER COMPONENT
 
@@ -25,8 +30,7 @@ Basket Button showing num of items in Basket (onClick goes to basket page)
 
 export const Header = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  const {cart, changeCart} = useContext(CartContext)
-
+  const { cart, changeCart } = useContext(CartContext);
 
   const [anchorProfileEl, setAnchorProfileEl] = useState(null);
 
@@ -34,13 +38,11 @@ export const Header = () => {
     setAnchorProfileEl(event.currentTarget);
   };
 
-
   const handleProfileClose = () => {
     setAnchorProfileEl(null);
   };
 
   const openProfile = Boolean(anchorProfileEl);
-
 
   const [anchorBasketEl, setAnchorBasketEl] = useState(null);
 
@@ -48,14 +50,12 @@ export const Header = () => {
     setAnchorBasketEl(event.currentTarget);
   };
 
-
   const handleBasketClose = () => {
     setAnchorBasketEl(null);
   };
 
   const openBasket = Boolean(anchorBasketEl);
 
-  
   useEffect(() => {
     fetchCart(changeCart);
   }, []);
@@ -64,27 +64,26 @@ export const Header = () => {
     console.log(cart);
   }, [cart]);
 
-  const updateQty =(id, newQuantity)=>{
-      updateCartQty(id, newQuantity, changeCart)
-  }
+  const updateQty = (id, newQuantity) => {
+    updateCartQty(id, newQuantity, changeCart);
+  };
 
-
-
-
-  const handledarkmodeChange = () =>{}
+  const handledarkmodeChange = () => {};
   return (
     <div
       style={{
         position: "sticky",
         top: "0",
+        // backgroundColor: "rgba(0,0,0,0.6)",
         backgroundColor: "#fff",
+        boxShadow: "rgb(0 0 0) 0px -17px 26px",
         padding: "10px 0",
         margin: "0",
-        zIndex:'2',
-        
+        zIndex: "2",
+        color: "white !important",
       }}
     >
-      <nav>
+      <nav style={{ zIndex: "2" }}>
         <ul
           style={{
             display: "flex",
@@ -102,7 +101,7 @@ export const Header = () => {
           <li>
             <Link to="/findastore">Find a Nearby Store</Link>
           </li>
- 
+
           <li>
             <ul
               style={{
@@ -113,56 +112,53 @@ export const Header = () => {
               }}
             >
               <li>
-                
                 <Tooltip title="Dark Mode On/Off">
                   <Switch
-            checked={false}
-            onChange={handledarkmodeChange}
-            name="darkMode"
-            color="primary"
-          />
+                    checked={false}
+                    onChange={handledarkmodeChange}
+                    name="darkMode"
+                    color="primary"
+                  />
                 </Tooltip>
-                
               </li>
 
               {isAuthenticated ? (
                 <li>
                   <Tooltip title="Profile">
-
-                      <IconButton aria-label="profile" onClick={handleProfileClick}>
-                      
-                        {typeof user.picture ?(
-                        
-                        <Avatar src={user?.picture} alt={user?.name} style={{height:'25px', width:'25px'}} />
-                        ):(
-                          <AccountCircleIcon />
-                        )}
-                        
-                      </IconButton>
+                    <IconButton
+                      aria-label="profile"
+                      onClick={handleProfileClick}
+                    >
+                      {typeof user.picture ? (
+                        <Avatar
+                          src={user?.picture}
+                          alt={user?.name}
+                          style={{ height: "25px", width: "25px" }}
+                        />
+                      ) : (
+                        <AccountCircleIcon />
+                      )}
+                    </IconButton>
                   </Tooltip>
 
                   <Popover
-                  open={openProfile}
-                  anchorEl={anchorProfileEl}
-                  onClose={handleProfileClose}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-          horizontal: 'center',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                  }}
+                    open={openProfile}
+                    anchorEl={anchorProfileEl}
+                    onClose={handleProfileClose}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "center",
+                    }}
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "center",
+                    }}
                   >
-                    <div style={{padding:'5px'}}>
+                    <div style={{ padding: "5px" }}>
                       <p>Contents</p>
-                    <Link to="/profile">
-                      Profile
-                    </Link>
-                    <LogoutButton/>
-                  
+                      <Link to="/profile">Profile</Link>
+                      <LogoutButton />
                     </div>
-                    
                   </Popover>
                 </li>
               ) : (
@@ -173,93 +169,180 @@ export const Header = () => {
 
               <li>
                 <Tooltip title="Basket">
-                    <IconButton aria-label="cart" onClick={handleBasketClick}>
-                      <Badge badgeContent={cart.total_items} color="primary">
-                        <ShoppingBasketIcon />
-                      </Badge>
-                    </IconButton>
+                  <IconButton aria-label="cart" onClick={handleBasketClick}>
+                    <Badge badgeContent={cart.total_items} color="primary">
+                      <ShoppingBasketIcon />
+                    </Badge>
+                  </IconButton>
                 </Tooltip>
-                
+
                 <Popover
                   open={openBasket}
                   anchorEl={anchorBasketEl}
                   onClose={handleBasketClose}
                   anchorOrigin={{
-                    vertical: 'bottom',
-          horizontal: 'center',
+                    vertical: "bottom",
+                    horizontal: "center",
                   }}
                   transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
+                    vertical: "top",
+                    horizontal: "center",
                   }}
-                  >
-                    <h1>Shopping cart</h1>
-      <hr />
-      <ul style={{listStyle:'none',display:'grid', gap:'50px', padding:'0', maxWidth:'100%'}}>
-        {cart?.line_items?.length === 0? (
-          <div>
-            <h3>Your Basket is empty</h3>
-            <p>Continue shopping on the <a href="">homepage</a>, browse our discounts, or visit your Wish List</p>
-          </div>
-          
-        ):(
-          <div>
-            {cart?.line_items?.map(item => (
-              
-          <li key={item.id}>
-            <div style={{display:'flex', justifyContent:'flex-start', gap:'20px'}}>
-              <div style={{backgroundColor:'blue', height:'100px', width:'100px', position:'relative', overflow:'hidden'}}>
-                <Link to={{
-                pathname: "/product",
-                search: `?id=${item.product_id}`,
-              }}
-              onClick={handleBasketClose}
-              >
-                  <img src={item?.media?.source} width="100"alt="" style={{position:'absolute', top:'0', left:'0', bottom:'0', objectFit:'cover', right:'0'}}/>
-                </Link>
+                >
+                  <div style={{ padding: "10px" }}>
+                    <div
+                      style={{
+                        position: "sticky",
+                        top: "0px",
+                        backgroundColor: "#fff",
+                        zIndex: "5",
+                      }}
+                    >
+                      <h1>Shopping Cart</h1>
+                      <hr />
+                    </div>
 
-              </div>
-                
-                <div style={{display:'grid'}}>
-                 <h3 style={{margin:'0', width:'fit-content'}}>{item.name}</h3>    
-<p style={{margin:'0', width:'fit-content'}}>{item.price?.formatted_with_symbol}</p>    
-            <p style={{margin:'0', width:'fit-content'}}>Quantity: {item.quantity}</p>
-            <div style={{width:'fit-content'}}>
-              <Tooltip title="Add 1">
-                <Button onClick={()=>updateQty(item.id, item.quantity+1)}>+</Button>
-              </Tooltip>
-              <Tooltip title="Remove 1">
-                <Button onClick={()=>updateQty(item.id, item.quantity-1)}>-</Button> 
-              </Tooltip>
-              <Tooltip title="Remove Item">
-                <IconButton onClick={()=>updateQty(item.id, 0)}>
-                  <DeleteIcon fontSize="small"/>
-                </IconButton>
-                
-              </Tooltip>
-            </div>
-            
-                </div>
-            
-            
-            
-            </div>
-            {/* <hr style={{margin:'0 0 5px'}}/> */}
-          </li>
-        ))}
-          </div>
-        )}
-        
-      </ul>
-      <hr />
-      <div  style={{margin:'auto', padding:' 5px 0 10px', width:'fit-content', boxSizing:'border-box'}}>
- <Link to="basket" onClick={handleBasketClose}>
-                      Go To Basket
-                    </Link>
-      </div>
-                   
-               
-                  </Popover>
+                    <ul
+                      style={{
+                        listStyle: "none",
+                        display: "grid",
+                        gap: "50px",
+                        padding: "0",
+                        maxWidth: "100%",
+                      }}
+                    >
+                      {cart?.line_items?.length === 0 ? (
+                        <div>
+                          <h3>Your Basket is empty</h3>
+                          <p>
+                            Continue shopping on the <a href="">homepage</a>,
+                            browse our discounts, or visit your Wish List
+                          </p>
+                        </div>
+                      ) : (
+                        <div>
+                          {cart?.line_items?.map((item) => (
+                            <li key={item.id}>
+                              <div
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "flex-start",
+                                  gap: "20px",
+                                }}
+                              >
+                                <div
+                                  style={{
+                                    backgroundColor: "blue",
+                                    height: "100px",
+                                    width: "100px",
+                                    position: "relative",
+                                    overflow: "hidden",
+                                  }}
+                                >
+                                  <Link
+                                    to={{
+                                      pathname: "/product",
+                                      search: `?id=${item.product_id}`,
+                                    }}
+                                    onClick={handleBasketClose}
+                                  >
+                                    <img
+                                      src={item?.media?.source}
+                                      width="100"
+                                      alt=""
+                                      style={{
+                                        position: "absolute",
+                                        top: "0",
+                                        left: "0",
+                                        bottom: "0",
+                                        objectFit: "cover",
+                                        right: "0",
+                                      }}
+                                    />
+                                  </Link>
+                                </div>
+
+                                <div style={{ display: "grid" }}>
+                                  <h3
+                                    style={{
+                                      margin: "0",
+                                      width: "fit-content",
+                                    }}
+                                  >
+                                    {item.name}
+                                  </h3>
+                                  <p
+                                    style={{
+                                      margin: "0",
+                                      width: "fit-content",
+                                    }}
+                                  >
+                                    {item.price?.formatted_with_symbol}
+                                  </p>
+                                  <p
+                                    style={{
+                                      margin: "0",
+                                      width: "fit-content",
+                                    }}
+                                  >
+                                    Quantity: {item.quantity}
+                                  </p>
+                                  <div style={{ width: "fit-content" }}>
+                                    <Tooltip title="Add 1">
+                                      <Button
+                                        onClick={() =>
+                                          updateQty(item.id, item.quantity + 1)
+                                        }
+                                      >
+                                        +
+                                      </Button>
+                                    </Tooltip>
+                                    <Tooltip title="Remove 1">
+                                      <Button
+                                        onClick={() =>
+                                          updateQty(item.id, item.quantity - 1)
+                                        }
+                                      >
+                                        -
+                                      </Button>
+                                    </Tooltip>
+                                    <Tooltip title="Remove Item">
+                                      <IconButton
+                                        onClick={() => updateQty(item.id, 0)}
+                                      >
+                                        <DeleteIcon fontSize="small" />
+                                      </IconButton>
+                                    </Tooltip>
+                                  </div>
+                                </div>
+                              </div>
+                              {/* <hr style={{margin:'0 0 5px'}}/> */}
+                            </li>
+                          ))}
+                        </div>
+                      )}
+                    </ul>
+                    <div style={{position:'sticky', bottom:'0', backgroundColor:'#fff'}}>
+                    <hr />
+                    <div
+                      style={{
+                        margin: "auto",
+                        padding: " 5px 0 10px",
+                        width: "fit-content",
+                        boxSizing: "border-box",
+                        textAlign:'center',
+                      }}
+                    >
+                     
+                      <p style={{margin:'5px 0'}}>SubTotal() items()</p>
+                      <Link to="basket" onClick={handleBasketClose}>
+                        Go To Basket
+                      </Link>
+                    </div>  
+                    </div>
+                    
+                  </div>
+                </Popover>
               </li>
             </ul>
           </li>
