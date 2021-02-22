82a431f5ff3782724184470e6f1258d8cc87ce81
Pudderz <mpudney6@gmail.com>
2021-02-22 22:40:43 +0000

    removed selectSize inline styles

--- Frontend/src/components/SelectSize.js
+++ Frontend/src/components/SelectSize.js
@@ -14,30 +14,22 @@ export const SelectSize = ({ availableSizes, changeSize,size }) => {
   }, [size])
 
   const handleButtonChange = (e)=>{
-    changeSize(e.target.textContent);
+    changeSize(1*e.target.textContent);
   }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexFlow: "wrap",
-        width: "400px",
-        borderLeft: "1px solid #ebedee",
-        borderTop: "1px solid #ebedee",
-        margin: "10px 0 20px",
-        fontSize:'18px'
-      }}
+    <div className="selectSize"
+ 
     >
       {ALL_SIZES.map((number, index) => {
         // Tests if that size is in the availableSizes object
-        // If not an avaliable size disable button
+        // If not sets avaliable size button to disabled
 
           return (
             <Button
               key={index}
               disabled={!(number in availableSizes)}
-              className={`availableSize ${(number==selectedSize) && 'selected'}`}
+              className={`availableSize ${(number===selectedSize) && 'selected'}`}
               onClick={handleButtonChange}
               style={{
                 border: "1px solid #ebedee",
