965cdbe030ab8a9c5d48e58089fa14638f8ae293
Pudderz <mpudney6@gmail.com>
2021-02-23 23:49:23 +0000

    added env info to next.config.js

---
+++ nextjsfrontend/next.config.js
@@ -0,0 +1,18 @@
+const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins');
+
+module.exports = withPlugins([
+    [withImages()]
+],{
+    env: {
+        REACT_APP_CHEC_PUBLIC_KEY : process.env.REACT_APP_CHEC_PUBLIC_KEY,
+        STRIPE_SECRET_API_KEY : process.env.STRIPE_SECRET_API_KEY,
+        REACT_APP_AUTH0_DOMAIN : process.env.REACT_APP_AUTH0_DOMAIN,
+        REACT_APP_AUTH0_CLIENT_ID : process.env.REACT_APP_AUTH0_CLIENT_ID,
+        REACT_APP_AUTH0_CLIENT_ID2 : process.env.REACT_APP_AUTH0_CLIENT_ID2,
+        APOLLO_KEY : process.env.APOLLO_KEY,
+        APOLLO_GRAPH_VARIANT : process.env.APOLLO_GRAPH_VARIANT,
+        APOLLO_SCHEMA_REPORTING : process.env.APOLLO_SCHEMA_REPORTING,
+        REACT_APP_AUTH0_AUDIENCE: process.env.REACT_APP_AUTH0_AUDIENCE,
+    },
+  });
