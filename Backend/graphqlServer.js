4cd8ae428f91c078640ccae8f0420f8c999275fb
Pudderz <mpudney6@gmail.com>
2021-02-28 11:12:06 +0000

    Added trending section to backend
    grabs most popular product pages using google analytics and google reporting API.

--- Backend/graphqlServer.js
+++ Backend/graphqlServer.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const PORT = 6969;
 const cors = require('cors');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./Schemas');
 const mongoose = require('mongoose');
+const {SimpleGA, Request} = require('node-simple-ga')
 const isTokenValid = require('./Authenication/validate');
 let database = null;
+
+let GOOGLE_PAGEPATHS = {};
+
 
 require("dotenv").config();
 const uri = `mongodb+srv://dbAdmin:${process.env.MONGODB_PASSWORD}@cluster0.s5qsx.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`;
@@ -51,6 +56,47 @@ app.use('/graphql', graphqlHTTP(async req=>({
 })))
 
 
+app.use('/trending', (req, res)=>{
+    (async function() {
+        var analytics = new SimpleGA(path.join(__dirname, "./key.json"));
+    
+        var requestPopularProducts = Request()
+            .select("pagepath","pageviews")
+            .from(process.env.GA_VIEW_ID)
+            .where("pagepath")
+            .beginsWith('/product')
+            .orderDesc("pageviews")
+            .limit(20);
+    
+    
+        try {
+    
+            var r1 = analytics.run(requestPopularProducts);
+
+            var [requestPopularProducts] = await Promise.all([
+                r1
+            ]);
+    
+            console.log(requestPopularProducts);
+            
+            const slugs = [];
+            requestPopularProducts.forEach(element => {
+                let slug= element.pagePath.slice(9);
+                if(slug != null){
+                    slugs.push({slug,pageviews: element.pageviews })
+                }    
+            });
+
+            console.log(slugs);
+            res.send(slugs);
+            GOOGLE_PAGEPATHS = slugs;
+        } catch (err) {
+            console.error(err);
+        }
+    })();
+})
+
+
 app.listen(PORT, () => {
-    console.log('Server running')
+    console.log(`Server running on port ${PORT}`)
 })
