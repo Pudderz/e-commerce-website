f79b9266b8e4f9a87b1dbc0223a02cc7641020d1
Pudderz <mpudney6@gmail.com>
2021-03-03 23:32:47 +0000

    Added deleteReviews and editReviews to schema

--- Backend/Schemas/index.js
+++ Backend/Schemas/index.js
@@ -10,10 +10,11 @@ const {
   GraphQLNonNull,
 } = graphql;
 const { ProductType, ProductReviews } = require("./TypeDefs/UserType");
-const reviewData = require("../reviewDataExample.json");
 const isTokenValid = require("../Authenication/validate");
 const Review = require("../models/review");
 const Product = require("../models/products");
+const Order = require("../models/order");
+const { ObjectId } = require("mongodb");
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -30,13 +31,6 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return Review.find({});
-      },
-    },
-    getUserReviews: {
-      type: new GraphQLList(ProductReviews),
-      args: { id: { type: GraphQLID }, name: { type: GraphQLString } },
-      resolve(parent, args) {
-        return Review.find({ name: args.name });
       },
     },
     getProduct:{
@@ -100,11 +94,6 @@ const Mutation = new GraphQLObjectType({
         productName: { type: new GraphQLNonNull(GraphQLString) },
       },
       resolve: async (parent, args, context) => {
-        // reviewData.push({
-        //   id: reviewData.length + 1,
-        //   firstName: args.firstName,
-        //   lastName: args.lastName,
-        // });
         const { db, token } = await context();
         // Tests if user is authenticated to create a review
         const { error } = await isTokenValid(token);
@@ -133,7 +122,7 @@ const Mutation = new GraphQLObjectType({
         description: { type: GraphQLString },
       },
       resolve: async (parent, args, context) => {
-        const { db, token } = await context();
+        const { db, token, subId } = await context();
         // Tests if user is authenticated to create a review
         let { error } = await isTokenValid(token);
         console.log(`is Authenicated - ${!error}`);
@@ -143,8 +132,12 @@ const Mutation = new GraphQLObjectType({
         //Return null if not authenticated
         if (error) return null;
 
+        // Return null if there is no subId to identify the user
+        if (!subId) return null;
+
         let review = new Review({
           productId: args.productId,
+          subId: subId,
           productName: args.productName,
           name: args.name,
           profileImage: args.profileImage,
@@ -156,17 +149,10 @@ const Mutation = new GraphQLObjectType({
 
         let product;
         await Product.findOne({ productId: args.productId }, function (err, obj) {
-          
           product = obj;
-          
         });
         
 
-        console.log(product);
-        console.log(!!product);
-         console.log(mongoose.connection.readyState=== 1);
-         console.log(typeof product === 'undefined');
-         console.log(product === product);
 
         if((typeof product === 'undefined' || !product ) && mongoose.connection.readyState=== 1){
           console.log(`${ args.productId} not found`)
@@ -207,50 +193,34 @@ const rating = (1 * product.averageRating * product.numOfReviews +
         }
 
         return !error ? review.save() : null;
-        // for(product in reviewData){
-        //   if(product.productId === args.productId){
-        //     product.allProductReviews.push({
-        //       id:product.allProductReviews.length+1,
-        //       name:args.name,
-        //       rating: args.rating,
-        //       descriptionTitle:args.description,
-        //       description: args.descriptionTitle,
-        //     })
-
-        //     reviewData[i].averageRating = (reviewData[i].averageRating*reviewData[i].numOfReviews+args.rating/(reviewData[i].numOfReviews+1))
-        //     reviewData[i].numOfReviews ++;
-        //     match = true;
-        //     break;
-        //   }
-        //   i++;
-        // }
-        // if(!match){
-
-        //   console.log(`is Authenicated - ${!error}`)
-        //   console.log(error)
-        // if(!error){
-
-        //   reviewData[0].allProductReviews.push({
-        //   id: reviewData[0].allProductReviews.length + 1,
-        //   name: args.name,
-        //   rating: args.rating,
-        //   descriptionTitle: args.description,
-        //   description: args.descriptionTitle,
-        //   profileImage: args.profileImage,
-        // });
-        // // }
-
-        // reviewData[0].averageRating =
-        //   (1 * reviewData[0].averageRating * reviewData[0].numOfReviews +
-        //     1 * args.rating) /
-        //   (reviewData[0].numOfReviews + 1);
-        // reviewData[0].numOfReviews = reviewData[0].allProductReviews.length;
-        // console.log("added review");
-        // }
-
-        // return (!error)? args: null;
+     
       },
     },
+    deleteReview: {
+      type: ProductType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve: async (parent, args, context) => {
+
+        return Review.findOneAndDelete(ObjectId(args.id))
+      }
+    },
+    editReview: {
+      type: ProductType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve: async (parent, args, context) => {
+        
+        return Review.findOneAndUpdate(ObjectId(args.id), {$set:{}}, {new: true}, (err,doc)=>{
+          if(err){
+            console.log("Something wrong when updating data!");
+          }
+          console.log(doc)
+        })
+      }
+    }
   },
 });
 
