211d08e39840abcc8a531633c8f4bc348bc7ec8c
Pudderz <mpudney6@gmail.com>
2021-03-03 23:34:36 +0000

    Added queries for getting user reviews and orders for the profile page

--- nextjsfrontend/GraphQL/Queries.js
+++ nextjsfrontend/GraphQL/Queries.js
@@ -1,19 +1,43 @@
-import {gql} from '@apollo/client'
+import { gql } from "@apollo/client";
 
 export const LOAD_REVIEWS = gql`
-query($productId: String!, $productName: String!) {
+  query($productId: String!, $productName: String!) {
     getProduct(productId: $productId, productName: $productName) {
-      productId,
-      numOfReviews,
-      averageRating,
+      productId
+      numOfReviews
+      averageRating
       allProductReviews {
-        name,
-        rating,
-        descriptionTitle,
-        description,
+        name
+        rating
+        descriptionTitle
+        description
         profileImage
       }
     }
   }
-  
-`
+`;
+
+export const LOAD_USER_REVIEWS = gql`
+  query {
+    getUserReviews {
+      _id
+      productId
+      name
+      rating
+      profileImage
+      productName
+      descriptionTitle
+      description
+    }
+  }
+`;
+
+export const LOAD_USER_ORDERS = gql`
+  query {
+    getUserOrders {
+      id
+      date
+      price
+    }
+  }
+`;
