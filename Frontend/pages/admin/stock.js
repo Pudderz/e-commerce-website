84d58426f2ad15cb8ffb9ae21c293b0bde60ad33
Pudderz <mpudney6@gmail.com>
2021-03-22 13:54:53 +0000

    Added basic stock select to stock page
    Not sure whether to expand on the stock page or just add a dropdown button onto allProducts admin page to quickly update the stock.

--- Frontend/pages/admin/stock.js
+++ Frontend/pages/admin/stock.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { ChangeStock } from '../../components/Admin/ChangeStock';
 // gets all products names and stock
 // lists all the products
 
 
+
+
 export const Stock = () => {
+
+    const [stockArray, setStockArray] = useState(Array.from({length: ALL_SIZES.length}, ()=> 0))
+  const handleStockChange = (index, value)=>{
+    let newValue = stockArray[index] + value
+    stockArray[index] = newValue>=0? newValue: 0;
+    setStockArray([...stockArray]);
+  }
+
+
     return (
         <div>
             <h2>Stock Page</h2>
+        
+            <ChangeStock availableSizes={[4]} stockArray={stockArray} changeStock={handleStockChange}/>
+
+
         </div>
     )
 }
