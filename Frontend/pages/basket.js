240b07a0f51c8c6ac99d4145f7861f4fd731854b
Pudderz <mpudney6@gmail.com>
2021-03-30 16:23:39 +0100

    updating and removing products on basket page effect redux store instead of commerce.js

--- Frontend/pages/basket.js
+++ Frontend/pages/basket.js
@@ -3,28 +3,30 @@ import React, { useEffect, useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Link from "next/link";
-import {addCartItem, removeCartItem} from "../Redux/actions/actions"
-import { connect } from 'react-redux'
+import {
+  addCartItem,
+  removeCartItem,
+  addCartItemQuantity,
+} from "../Redux/actions/actions";
+import { connect } from "react-redux";
 
 // Shows a list of items added to the basket page
 
 //shows total price
 export const Basket = (props) => {
-  console.log(props)
+  console.log(props);
 
-  const {cart: stateCart =[]} = props.state.cart;
+  const { cartState: stateCart = [], cartInfo } = props;
   const { cart, removeFromCart, updateQty } = useContext(CartContext);
-  console.log(stateCart)
-console.log(`stateCart ${typeof stateCart}`)
+  console.log(stateCart);
+  console.log(`stateCart ${typeof stateCart}`);
   useEffect(() => {
-    console.log(stateCart)
-    return () => {
-      
-    }
-  }, [stateCart])
-
+    console.log(stateCart);
+    return () => {};
+  }, [stateCart]);
 
   const [isSmallDisplay, setIsSmallDisplay] = useState(false);
+
   useEffect(() => {
     if (window.innerWidth < 700) {
       setIsSmallDisplay(true);
@@ -49,256 +51,277 @@ console.log(`stateCart ${typeof stateCart}`)
     };
   }, []);
 
-
- 
- 
-  const updateItemQty = (id, newQuantity) => {
-    updateQty(id, newQuantity);
+  const updateItemQty = (name, size, newQuantity) => {
+    console.log(name, size, newQuantity);
+    props.addCartItemQuantity({
+      name,
+      size,
+      quantity: newQuantity,
+    });
   };
 
-  const removeItem = (id)=> removeFromCart(id);
+  const removeItem = (name, size) => {
+    console.log(name, size);
+    props.removeCartItem({
+      name,
+      size,
+    });
+  };
 
-
-  useEffect(() => {
-    console.log(cart);
-  }, [cart]);
-
- 
   return (
     <div>
       <h1>Shopping cart</h1>
       <hr />
       {JSON.stringify(stateCart)}
-      {isSmallDisplay ?
-      (
+      {isSmallDisplay ? (
         <div style={{ padding: "10px" }}>
-        
-        <ul
-          style={{
-            listStyle: "none",
-            display: "grid",
-            gap: "50px",
-            padding: "0",
-            maxWidth: "100%",
-          }}
-        >
-          {stateCart?.length === 0 ? (
-            <div>
-              <h3>Your Basket is empty</h3>
-              <p>
-                Continue shopping on the{" "}
-                <Link href="/">homepage</Link>, browse our discounts,
-                or visit your Wish List
-              </p>
-            </div>
-          ) : (
-            <div>
-              {[...stateCart]?.map((item) => (
-                <li key={item.id}>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "flex-start",
-                      gap: "20px",
-                    }}
-                  >
+          <ul
+            style={{
+              listStyle: "none",
+              display: "grid",
+              gap: "50px",
+              padding: "0",
+              maxWidth: "100%",
+            }}
+          >
+            {stateCart?.length === 0 ? (
+              <div>
+                <h3>Your Basket is empty</h3>
+                <p>
+                  Continue shopping on the <Link href="/">homepage</Link>,
+                  browse our discounts, or visit your Wish List
+                </p>
+              </div>
+            ) : (
+              <div>
+                {stateCart?.map((item) => (
+                  <li key={item.id}>
                     <div
                       style={{
-                        backgroundColor: "blue",
-                        height: "100px",
-                        width: "100px",
-                        position: "relative",
-                        overflow: "hidden",
+                        display: "flex",
+                        justifyContent: "flex-start",
+                        gap: "20px",
                       }}
                     >
-                      <Link
-                        href={`/product/${item.permalink}`}
-                       
+                      <div
+                        style={{
+                          backgroundColor: "blue",
+                          height: "100px",
+                          width: "100px",
+                          position: "relative",
+                          overflow: "hidden",
+                        }}
                       >
-                        <img
-                          src={`${process.env.GOOGLE_CLOUD_PUBLIC_URL}${item?.images[0]}`}
-                          width="100"
-                          alt={item.name}
-                          style={{
-                            position: "absolute",
-                            top: "0",
-                            left: "0",
-                            bottom: "0",
-                            objectFit: "cover",
-                            right: "0",
-                          }}
-                        />
-                      </Link>
-                    </div>
+                        <Link href={`/product/${item.slug}`}>
+                          <img
+                            src={`${process.env.GOOGLE_CLOUD_PUBLIC_URL}${item?.images[0]}`}
+                            width="100"
+                            alt={item.name}
+                            style={{
+                              position: "absolute",
+                              top: "0",
+                              left: "0",
+                              bottom: "0",
+                              objectFit: "cover",
+                              cursor: "pointer",
+                              right: "0",
+                            }}
+                          />
+                        </Link>
+                      </div>
 
-                    <div style={{ display: "grid" }}>
+                      <div style={{ display: "grid" }}>
+                        <h3
+                          style={{
+                            margin: "0",
+                            width: "fit-content",
+                          }}
+                        >
+                          {item.name}
+                        </h3>
+                        <p
+                          style={{
+                            margin: "0",
+                            width: "fit-content",
+                          }}
+                        >
+                          UK {item?.size} {item?.price}
+                        </p>
+                        <p
+                          style={{
+                            margin: "0",
+                            width: "fit-content",
+                          }}
+                        >
+                          Quantity: {item.quantity}
+                        </p>
+                        <div style={{ width: "fit-content" }}>
+                          <Tooltip title="Add 1">
+                            <Button
+                              onClick={() =>
+                                updateItemQty(
+                                  item.name,
+                                  item.size,
+                                  item.quantity + 1
+                                )
+                              }
+                            >
+                              +
+                            </Button>
+                          </Tooltip>
+                          <Tooltip title="Remove 1">
+                            <Button
+                              onClick={() =>
+                                updateItemQty(
+                                  item.name,
+                                  item.size,
+                                  item.quantity - 1
+                                )
+                              }
+                            >
+                              -
+                            </Button>
+                          </Tooltip>
+                          <Tooltip title="Remove Item">
+                            <IconButton
+                              onClick={() => removeItem(item.name, item.size)}
+                            >
+                              <DeleteIcon fontSize="small" />
+                            </IconButton>
+                          </Tooltip>
+                        </div>
+                      </div>
+                    </div>
+                    {/* <hr style={{margin:'0 0 5px'}}/> */}
+                  </li>
+                ))}
+              </div>
+            )}
+          </ul>
+        </div>
+      ) : (
+        <table
+          style={{
+            width: "100%",
+            maxWidth: "1100px",
+            textAlign: "center",
+            margin: "auto",
+            overflow: "auto",
+          }}
+        >
+          <thead>
+            <tr>
+              <th className="tableHeader">Item</th>
+              <th className="tableHeader">Price</th>
+              <th className="tableHeader">Quantity</th>
+              <th className="tableHeader">Total Price</th>
+              <th className="tableHeader">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[...stateCart]?.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  <div className="shoppingCartItem">
+                    <div
+                      style={{
+                        height: "200px",
+                        maxWidth: "150px",
+                        width: "100%",
+                        position: "relative",
+                      }}
+                    >
+                      <img
+                        src={`${process.env.GOOGLE_CLOUD_PUBLIC_URL}${item?.images[0]}`}
+                        alt=""
+                      />
+                    </div>
+                    <div>
                       <h3
                         style={{
-                          margin: "0",
                           width: "fit-content",
+                          margin: "0",
+                          fontSize: "clamp(14px, 2vw, 20px)",
                         }}
                       >
                         {item.name}
                       </h3>
-                      <p
-                        style={{
-                          margin: "0",
-                          width: "fit-content",
-                        }}
-                      >
-                       UK {item?.size} {item?.price}
-                      </p>
-                      <p
-                        style={{
-                          margin: "0",
-                          width: "fit-content",
-                        }}
-                      >
-                        Quantity: {item.quantity}
-                      </p>
-                      <div style={{ width: "fit-content" }}>
-                        <Tooltip title="Add 1">
-                          <Button
-                            onClick={() =>
-                              updateItemQty(item.id, item.quantity + 1)
-                            }
-                          >
-                            +
-                          </Button>
-                        </Tooltip>
-                        <Tooltip title="Remove 1">
-                          <Button
-                            onClick={() =>
-                              updateItemQty(item.id, item.quantity - 1)
-                            }
-                          >
-                            -
-                          </Button>
-                          
-                        </Tooltip>
-                        <Tooltip title="Remove Item">
-                          <IconButton
-                            onClick={() => removeItem(item.id)}
-                          >
-                            <DeleteIcon fontSize="small" />
-                          </IconButton>
-                        </Tooltip>
-                      </div>
+                      <p style={{ textAlign: "start" }}>Size: {item?.size}</p>
                     </div>
                   </div>
-                  {/* <hr style={{margin:'0 0 5px'}}/> */}
-                </li>
-              ))}
-            </div>
-          )}
-        </ul>
-
-      </div>
-      ):(
-<table
-        style={{
-          width: "100%",
-          maxWidth: "1100px",
-          textAlign: "center",
-          margin: "auto",
-          overflow:'auto'
-        }}
-      >
-        <thead> 
-        <tr>
-          <th className="tableHeader">Item</th>
-          <th className="tableHeader">Price</th>
-          <th className="tableHeader">Quantity</th>
-          <th className="tableHeader">Total Price</th>
-          <th className="tableHeader">Remove</th>
-        </tr>
-        </thead> 
-        <tbody>
-
-       
-        {[...stateCart]?.map((item) => (
-          <tr key={item.id}>
-            <td>
-              <div
-              className="shoppingCartItem"
-               
-              >
-                <div
-                  style={{
-                    height: "200px",
-                    maxWidth: "150px",
-                    width: "100%",
-                    position: "relative",
-                  }}
-                >
-                  <img
-                    src={`${process.env.GOOGLE_CLOUD_PUBLIC_URL}${item?.images[0]}`}
-                    alt=""
-               
-                  />
-                </div>
-                <div>
-                  <h3
+                </td>
+                <td>
+                  <p>£{item.price}.00</p>
+                </td>
+                <td>
+                  <div style={{ margin: "0 auto", width: "fit-content" }}>
+                    {item.quantity == item.maxStock && (
+                      <p
+                        style={{
+                          margin: "0 auto",
+                          color: "red",
+                          width: "fit-content",
+                        }}
+                      >
+                        Max Stock
+                      </p>
+                    )}
+                  </div>
+                  <div
                     style={{
-                      width: "fit-content",
-                      margin: "0",
-                      fontSize: "clamp(14px, 2vw, 20px)",
+                      display: "flex",
+                      justifyContent: "center",
+                      height: "100%",
                     }}
                   >
-                    {item.name}
-                  </h3>
-                  <p style={{ textAlign: "start" }}>
-                    Size: {item?.size}
-                  </p>
-                </div>
-              </div>
-            </td>
-            <td>
-              <p>{item.price}</p>
-            </td>
-            <td>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  height: "100%",
-                }}
-              >
-                <Button
-                size="small"
-                  onClick={() => updateItemQty(item.id, item.quantity + 1)}
-                  style={{ padding: "0", width:'5px', minWidth:'20px', margin:'auto' }}
-                >
-                  +
-                </Button>
-                <p>{item.quantity}</p>
-                <Button
-                  onClick={() => updateItemQty(item.id, item.quantity - 1)}
-                  style={{ padding: "0", width:'5px', minWidth:'20px', margin:'auto' }}
-                >
-                  -
-                </Button>
-              </div>
-            </td>
-            <td>
-              <p>{`£${((item.price?.raw * 100 * item.quantity) / 100).toFixed(
-                2
-              )}`}</p>
-            </td>
-            <td>
-              <Tooltip title="Remove Item">
-                <IconButton onClick={() => updateItemQty(item.id, 0)}>
-                  <DeleteIcon fontSize="small" />
-                </IconButton>
-              </Tooltip>
-            </td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
-      
+                    <Button
+                      size="small"
+                      onClick={() =>
+                        updateItemQty(item.name, item.size, item.quantity + 1)
+                      }
+                      style={{
+                        padding: "0",
+                        width: "5px",
+                        minWidth: "20px",
+                        margin: "auto",
+                      }}
+                    >
+                      +
+                    </Button>
+                    <p>{item.quantity}</p>
+                    <Button
+                      onClick={() =>
+                        updateItemQty(item.name, item.size, item.quantity - 1)
+                      }
+                      style={{
+                        padding: "0",
+                        width: "5px",
+                        minWidth: "20px",
+                        margin: "auto",
+                      }}
+                    >
+                      -
+                    </Button>
+                  </div>
+                </td>
+                <td>
+                  <p>£{item.price * item.quantity}.00</p>
+                </td>
+                <td>
+                  <Tooltip title="Remove Item">
+                    <IconButton
+                      onClick={() => removeItem(item.name, item.size)}
+                    >
+                      <DeleteIcon fontSize="small" />
+                    </IconButton>
+                  </Tooltip>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
-      
+
       <div
         style={{
           position: "sticky",
@@ -311,8 +334,8 @@ console.log(`stateCart ${typeof stateCart}`)
       >
         <hr />
         <p>
-          Subtotal({cart?.total_items || 0} items):{" "}
-          {cart?.subtotal?.formatted_with_symbol}
+          Subtotal({cartInfo.totalItems || 0} items):{" "}
+          {cartInfo.totalPrice}
         </p>
         <Link href="/checkout">Proceed to checkout</Link>
       </div>
@@ -320,20 +343,19 @@ console.log(`stateCart ${typeof stateCart}`)
   );
 };
 
-
 const mapStateToProps = (state, ownProps) => ({
   // ... computed data from state and optionally ownProps
-  state:{...state},
-  props:{...ownProps},
-})
+  cartState: state.cart.cart,
+  cartInfo: state.cart.cartInfo,
+  props: { ...ownProps },
+});
 
 const mapDispatchToProps = {
   addCartItem,
   removeCartItem,
+  addCartItemQuantity,
   // ... normally is an object full of action creators
-}
+};
 
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Basket)
+export default connect(mapStateToProps, mapDispatchToProps)(Basket);
 // export default Basket;
