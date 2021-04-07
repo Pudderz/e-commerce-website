169163bf67c1be6a8d08fef9ab550131184e0a2b
Pudderz <mpudney6@gmail.com>
2021-04-07 23:42:56 +0100

    simplified authenticated routes with auth0 withAuthenticationRequired

--- Frontend/components/Authentication/AuthenticatedRoute.js
+++ Frontend/components/Authentication/AuthenticatedRoute.js
@@ -1,18 +1,23 @@
-import { Children, useContext } from "react";
-import React, { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import { useRouter } from "next/router";
-import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import { withAuthenticationRequired } from '@auth0/auth0-react';
 
-export const AuthenticatedRoute = ({ children }) => {
-  const router = useRouter();
-  const authContext = useContext(AuthContext);
-  const {user, isAuthenticated, isLoading} = useAuth0();
+const Redirecting = () =>{
+  return(
+    <div>Redirecting you to the login page...</div>
+  )
+} 
 
 
-  return isAuthenticated ? (
+const AuthenticatedRoute = ({ children }) => {
+
+  return (
     <div>{children}</div>
-  ) : (
-    <div>{loginWithRedirect({screen_hint: 'signup'})}</div>
-  );
+  ) 
 };
+
+
+
+export default withAuthenticationRequired(AuthenticatedRoute, {
+  // Show a message while the user waits to be redirected to the login page.
+  onRedirecting: () => (Redirecting)
+});
