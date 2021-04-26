3b96313e7ba0fc7986b65c91f09c24be29a4159b
Pudderz <mpudney6@gmail.com>
2021-04-26 20:14:57 +0100

    changed selectSize component design

--- Frontend/components/ProductPages/SelectSize.js
+++ Frontend/components/ProductPages/SelectSize.js
@@ -1,62 +1,104 @@
 import { Button } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+const ALL_SIZES = [
+  3.5,
+  4,
+  4.5,
+  5,
+  5.5,
+  6,
+  6.5,
+  7,
+  7.5,
+  8,
+  8.5,
+  9,
+  9.5,
+  10,
+  10.5,
+];
 
-const ALL_SIZES = [3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5];
+const SelectSizeWrapper = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  width: 400px;
+  margin: 10px 0 20px;
+  font-size: 18px;
+  gap: 5px;
+  @media (max-width: 1200px) {
+    width: 100%;
+  }
+`;
 
+const SizeButton = styled(Button)`
+  border: 1px solid #ebedee !important;
+  border-radius: 5px;
+  width: 20%;
+  &:hover {
+    background-color: #828488 !important;
+    color: #fff;
+    transition: 0.5s all;
+    -webkit-transition: 0.5s all;
+    -moz-transition: 0.5s all;
+    -ms-transition: 0.5s all;
+    -o-transition: 0.5s all;
+  }
+  &.selected {
+    background-color: #4d5461 !important;
+    color: white;
+  }
+  @media (max-width: 1200px) {
+    width: 49%;
+    &.disabled {
+    display:none;
+  }
+  }
+`;
 
-
-export const SelectSize = ({ availableSizes, changeSize,size }) => {
-  console.log(availableSizes)
-  const [selectedSize, setSelectedSize] = useState(null)
+export const SelectSize = ({ availableSizes, changeSize, size }) => {
+  console.log(availableSizes);
+  const [selectedSize, setSelectedSize] = useState(null);
 
   const [shoeSizes, setShoeSizes] = useState([]);
   useEffect(() => {
-    console.log(size)
+    console.log(size);
     setSelectedSize(size);
-  }, [size])
+  }, [size]);
 
   useEffect(() => {
     const stock = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    availableSizes.forEach(sizeObject=>{
-      let shoeIndex = (sizeObject.shoeSize/10 - 3.5)/0.5;
+    availableSizes.forEach((sizeObject) => {
+      let shoeIndex = (sizeObject.shoeSize / 10 - 3.5) / 0.5;
       stock[shoeIndex] = sizeObject.stock;
-    })
+    });
     setShoeSizes(stock);
-    
-  }, [availableSizes])
+  }, [availableSizes]);
 
-  const handleButtonChange = (e)=>{
-    changeSize(1*e.target.textContent);
-  }
+  const handleButtonChange = (e) => {
+    changeSize(1 * e.target.textContent);
+  };
 
   return (
-    <div className="selectSize"
- 
-    >
+    <SelectSizeWrapper>
       {shoeSizes?.map((number, index) => {
         // Tests if that size is in the availableSizes object
         // If not sets avaliable size button to disabled
 
-          return (
-            <Button
-              key={index}
-              disabled={!(1*shoeSizes[index])}
-              className={`availableSize ${(1*ALL_SIZES[index]===selectedSize) && 'selected'}`}
-              onClick={handleButtonChange}
-              style={{
-                border: "1px solid #ebedee",
-                borderRadius: "0",
-                width: "20%",
-                borderLeft: "none",
-                borderTop: "none",
-              }}
-            >
-              {ALL_SIZES[index]}
-            </Button>
-          );
-        
+        return (
+          <SizeButton
+            key={index}
+            disabled={!(1 * shoeSizes[index])}
+            className={`availableSize ${
+              1 * ALL_SIZES[index] === selectedSize && "selected"
+            } ${!(1 * shoeSizes[index]) && "disabled"}`}
+            onClick={handleButtonChange}
+          >
+            {ALL_SIZES[index]}
+          </SizeButton>
+        );
       })}
-    </div>
+    </SelectSizeWrapper>
   );
 };
 
