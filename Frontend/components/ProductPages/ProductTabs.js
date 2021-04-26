932533432974ad422f6e8c153dec50063a131c92
Pudderz <mpudney6@gmail.com>
2021-04-26 20:14:22 +0100

    Changed product page tabs design

--- Frontend/components/ProductPages/ProductTabs.js
+++ Frontend/components/ProductPages/ProductTabs.js
@@ -1,66 +1,46 @@
 import React, { useState, useEffect } from "react";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
 import { ReviewProduct } from "../Review/ReviewProduct";
 import { SizeGuide } from "./SizeGuide";
 import { DeliveryAndReturns } from "./DeliveryAndReturns";
-import { Tab, Tabs } from "@material-ui/core";
+import { Tab } from "./Tab";
 import ReactMarkdown from "react-markdown";
-import TabPanel from "../Common/TabPanel";
 
 export const ProductTabs = (props) => {
-  const [value, setValue] = useState(0);
-const [product, setProduct] = useState({});
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-    console.log(newValue);
-  };
+  const [product, setProduct] = useState({});
 
-useEffect(() => {
-  setProduct(props.product);
-  console.log(props.product);
-}, [props]);
+  useEffect(() => {
+    setProduct(props.product);
+  }, [props]);
 
   return (
     <div
       style={{
         position: "relative",
         textAlign: "initial",
-        maxWidth: "900px",
+        maxWidth: "1000px",
         margin: "auto",
       }}
     >
+      <Tab name={"Product Details"}>
+        <div>
+          <h2>{product?.name}</h2>
+          <ReactMarkdown children={product?.description} />
+        </div>
+      </Tab>
 
-      <Tabs
-        value={value}
-        onChange={handleChange}
-          variant="fullWidth"
-        aria-label="full width tabs of products information"
-      >
-        <Tab value={0} label="Description"></Tab>
-        <Tab value={1} label="Size Guide"></Tab>
-        <Tab value={2} label="Delivery & Returns"></Tab>
-        <Tab value={3} label="Reviews"></Tab>
-      </Tabs>
-
-        <TabPanel value={value} index={0}>
-          <div>
-            <h2>{product?.name}</h2>
-
-            <ReactMarkdown children={product?.description} />
-            
-          </div>
-        </TabPanel>
-
-        <TabPanel value={value} index={1}>
-          <SizeGuide/>
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-         <DeliveryAndReturns/>
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          <ReviewProduct product={product} productId={product?.id} productName={product?.name} />
-        </TabPanel>
+      <Tab name={"Size Guide"}>
+        <SizeGuide />
+      </Tab>
+      <Tab name={"Delivery & Returns"}>
+        <DeliveryAndReturns />
+      </Tab>
+      <Tab name={"Reviews"}>
+        <ReviewProduct
+          product={product}
+          productId={product?.id}
+          productName={product?.name}
+        />
+      </Tab>
     </div>
   );
 };
---
+++ Frontend/components/ProductPages/Tab.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Button } from "@material-ui/core";
+
+const TabHeader = styled(Button)`
+  width: 100%;
+  justify-content: space-between !important;
+  min-height: 60px;
+  border-radius: 5px;
+    position:sticky !important;
+    z-index:1;
+    top:52px;
+    background-color:#fff !important;
+    box-shadow: 0px 1px 5px rgb(0, 0, 0, 0.2);
+  span {
+    text-transform: none;
+    width: 100%;
+    text-align: start;
+    font-size: 1rem;
+  }
+  &:hover {
+    /* border: 1px solid black; */
+  }
+`;
+
+const TabWrapper = styled.div`
+  /* border: 1px solid black; */
+  /* padding: 20px; */
+  position:relative;
+  box-shadow: 0px 1px 5px rgb(0, 0, 0, 0.2);
+  border: 1px solid #d6d6d6;
+  margin: 10px;
+  border-radius: 5px;
+  div:nth-child(1) {
+    transition: 1s all;
+    padding:20px;
+    
+  }
+  .hide {
+      height: 0px;
+      padding:0px;
+      overflow: hidden;
+      transition: 1s all;
+    }
+`;
+export const Tab = ({ name, children }) => {
+  const [min, setMin] = useState(true);
+
+  const handleMinimize = () => setMin((prevState) => !prevState);
+
+  return (
+    <TabWrapper>
+      <TabHeader onClick={handleMinimize}>{name}</TabHeader>
+      <div className={min && "hide"} >{children}</div>
+    </TabWrapper>
+  );
+};
+
+export default Tab;
