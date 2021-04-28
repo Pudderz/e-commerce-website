ee6b4df388416ba474dbb3e1dfad36dd97500ea8
Pudderz <mpudney6@gmail.com>
2021-04-28 14:40:49 +0100

    updated product page design

--- Frontend/components/ProductPages/ProductPageContent.js
+++ Frontend/components/ProductPages/ProductPageContent.js
@@ -1,20 +1,18 @@
-import { Breadcrumbs, Button, Typography } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import React, { useEffect, useState, useRef } from "react";
-import Link from "next/link";
 import { ProductImages } from "./ProductImages";
 import { ProductTabs } from "./ProductTabs";
 import { RecentlyViewed } from "../../components/Common/RecentlyViewed";
 import { addToHistory } from "../../lib/localStorage";
 import { useSnackbar } from "notistack";
 import { SelectSize } from "../../components/ProductPages/SelectSize";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { useRouter } from "next/router";
 import DefaultErrorPage from "next/error";
-import SelectSmallSize from "../../components/ProductPages/SelectSizeSmall";
 import { addCartItem, removeCartItem } from "../../Redux/actions/actions";
 import { connect } from "react-redux";
 import { AddToCart } from "lib/cart";
 import styled from "styled-components";
+import Rating from "@material-ui/lab/Rating";
 
 const Wrapper = styled.div`
   display: flex;
@@ -28,18 +26,18 @@ const Wrapper = styled.div`
 
 const ItemHeaderInfo = styled.div`
   max-width: 80%;
-  padding: 0 40px;
+  padding: 40px 40px;
   @media (max-width: 1200px) {
-    .selectSize {
+    /* .selectSize {
       width: 100%;
-    }
+    } */
     margin: auto;
     max-width: 100%;
     padding: 0;
     width: 90%;
-    button {
+    /* button {
       width: 50%;
-    }
+    } */
   }
 `;
 
@@ -47,7 +45,7 @@ export const ProductPageContent = (props) => {
   const [product, setProduct] = useState({});
   const [size, setSize] = useState("");
   const { enqueueSnackbar } = useSnackbar();
-
+  const addedToHistoryRef = useRef(false);
   const router = useRouter();
 
   // Fallback
@@ -58,16 +56,12 @@ export const ProductPageContent = (props) => {
     return <DefaultErrorPage statusCode={404} />;
   }
 
-  const addedToHistoryRef = useRef(false);
-  useEffect(() => {
-    setProduct(props.props);
-  }, [props.props]);
-
   useEffect(() => {
     if (props.props && !addedToHistoryRef.current) {
       addToHistory(props.props);
       addedToHistoryRef.current = true;
     }
+    setProduct(props.props);
   }, [props.props]);
 
   const handleAddToCart = (item, quantity = 1) => {
@@ -80,22 +74,28 @@ export const ProductPageContent = (props) => {
     }
   };
 
-  const changeSize = (size) => {
-    setSize(size);
-  };
+  const changeSize = (size) => setSize(size);
 
   return (
-    <div style={{ padding: "0 20px" }}>
+    <div style={{ padding: "0 5px" }}>
       <Wrapper>
         <ProductImages images={props.assets || props.images} />
 
-        <ItemHeaderInfo >
+        <ItemHeaderInfo>
           <h1>{product?.name}</h1>
-          <h2>£{(props?.price / 100).toFixed(2)}</h2>
-          <p style={{ color: "green" }}>In Stock</p>
+          <Rating
+            precision={0.1}
+            defaultValue={4}
+            readOnly
+            style={{ color: "#111" }}
+          />
+          <h2 style={{ margin: "20px 0", fontSize: "25px" }}>
+            £{(props?.price / 100).toFixed(2)}
+          </h2>
 
-          <label htmlFor="sizes" style={{ fontWeight: "bold" }}>
-            Select size
+          <p style={{ color: "green", margin: "0 0 20px" }}>In Stock</p>
+          <label htmlFor="sizes" style={{ color: "#888", fontSize: "14px" }}>
+            Select size . Size Guide
           </label>
           <SelectSize
             availableSizes={props?.stock}
@@ -107,17 +107,25 @@ export const ProductPageContent = (props) => {
             <Button
               onClick={() => handleAddToCart(product, 1)}
               variant="contained"
-              startIcon={<ShoppingCartIcon />}
+              // startIcon={<ShoppingCartIcon />}
               style={{
-                backgroundColor: "#bc2334",
+                // backgroundColor: "#bc2334",
+                backgroundColor: "#111",
+                textTransform: "none",
+                fontSize: "15px",
                 color: "white",
-                width: "60%",
+                width: "100%",
               }}
             >
-              ADD TO BASKET
+              Add to Basket
             </Button>
           </div>
-        </ItemHeaderInfo >
+          <div style={{ display: "grid", padding: "20px 0", gap: "5px" }}>
+            <a href="#details">Product Details</a>
+            <a href="#returns">Delivery & Return</a>
+            <a href="#reviews">Reviews</a>
+          </div>
+        </ItemHeaderInfo>
       </Wrapper>
 
       {/* <div className="itemNav">
